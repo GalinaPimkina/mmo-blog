f5294491fbf1c9dbc3169124bac3acb706fd3e18
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-03 13:52:43 +0300

    add models Post, Category, Comment

--- mmo/ads/models.py
+++ mmo/ads/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class User(AbstractUser):
     avatar = models.ImageField(upload_to="avatars/%Y/%m/%d/", default=None, blank=True, null=True, verbose_name="Avatar")
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Title")
+    content = models.TextField() # д/б текст, картинки, видео и тд
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="post", verbose_name="Category")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Category")
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment", verbose_name="Comment")
