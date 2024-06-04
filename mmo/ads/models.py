f02cb0f4c9af8682db640814218342b2eaa9af5b
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-04 12:45:35 +0300

    add field nickname

--- mmo/ads/models.py
+++ mmo/ads/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    nickname = models.CharField(max_length=50, verbose_name="Nickname")
     avatar = models.ImageField(upload_to="avatars/%Y/%m/%d/", default=None, blank=True, null=True, verbose_name="Avatar")
 
 
