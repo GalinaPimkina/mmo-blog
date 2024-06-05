92f1f814ba107ef4528d51ef271c9c655616bb9e
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-05 14:05:55 +0300

    register models in admin panel

--- mmo/ads/admin.py
+++ mmo/ads/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Post, Category, Comment, News
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(News)
