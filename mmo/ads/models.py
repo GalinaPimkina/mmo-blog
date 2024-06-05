e23e34576b64df61e47f09ae9d45c7f8c5d7d470
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-05 14:48:12 +0300

    add fields

--- mmo/ads/models.py
+++ mmo/ads/models.py
@@ -10,18 +10,39 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="Title")
     content = models.TextField() # д/б текст, картинки, видео и тд
     category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="post", verbose_name="Category")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="post", verbose_name="Author")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+
+    def __str__(self):
+        return self.title
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name="Category")
     icon = models.ImageField(upload_to="icons/", verbose_name="Icon")
 
+    def __str__(self):
+        return self.name
+
 
 class Comment(models.Model):
     content = models.TextField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment", verbose_name="Comment")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="comment", verbose_name="Author")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+
+    def __str__(self):
+        return f"{self.time_create} - {self.post}"
 
 
 class News(models.Model):
     title = models.CharField(max_length=255, verbose_name='Title')
-    content = models.TextField() # д.б текст картинки видео и тд
+    content = models.TextField(verbose_name="Content") # д.б текст картинки видео и тд
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="news", verbose_name="Author")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+
+    def __str__(self):
+        return f"{self.time_create} - {self.title}"
