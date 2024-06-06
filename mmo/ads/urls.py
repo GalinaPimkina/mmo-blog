04900db83812bf4ca040a6ed1d1ff021b02b4233
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-06 14:41:21 +0300

    маршрут для категорий

--- mmo/ads/urls.py
+++ mmo/ads/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'ads'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('categories/', views.CategoryView.as_view(), name='categories'),
 ]
