8f374d5f6bba924e5cde27742520a9b853cbbad0
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-10 14:27:38 +0300

    доб. слаги в таблицы

--- mmo/ads/models.py
+++ mmo/ads/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from autoslug import AutoSlugField
+
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=50, verbose_name="Nickname")
     avatar = models.ImageField(upload_to="avatars/%Y/%m/%d/", default=None, blank=True, null=True, verbose_name="Avatar")
+
+    def __str__(self):
+        return self.nickname
 
 
 class Post(models.Model):
@@ -13,6 +18,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="post", verbose_name="Author")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+    post_slug = AutoSlugField(populate_from='title', db_index=True, unique=True, verbose_name='Slug')
 
     def __str__(self):
         return self.title
@@ -21,6 +27,7 @@ class Post(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name="Category")
     icon = models.ImageField(upload_to="icons/", verbose_name="Icon")
+    category_slug = AutoSlugField(populate_from='name', db_index=True, unique=True, verbose_name='Slug')
 
     def __str__(self):
         return self.name
@@ -43,6 +50,7 @@ class News(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="news", verbose_name="Author")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+    news_slug = AutoSlugField(populate_from='title', db_index=True, unique=True, verbose_name='Slug')
 
     def __str__(self):
         return f"{self.time_create} - {self.title}"
