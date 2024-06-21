3da770fe3e581b702fd8180e74adc7c3a1fc892a
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-06-21 15:26:57 +0300

    меню в base.html

--- mmo/ads/utils.py
+++ mmo/ads/utils.py
@@ -1,13 +1,4 @@
-menu = [
-    {'title': 'Главная страница', 'url_name': 'ads:index'},
-    {'title': 'Добавить новость', 'url_name': 'ads:add_news'},
-    {'title': 'Поиск пати', 'url_name': 'ads:all_posts'},
-    {'title': 'Создать заявку', 'url_name': 'ads:add_post'},
-]
-
-
 class DataMixin:
     def get_mixin_context(self, context, **kwargs):
-        context['menu'] = menu
         context.update(kwargs)
         return context
