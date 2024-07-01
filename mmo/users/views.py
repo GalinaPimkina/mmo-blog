9cd55a7363a657c1b37015d8fa7fb7d45d4775db
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-07-01 16:05:31 +0300

    добавлен параметр enctype="multipart/form-data"

--- mmo/users/views.py
+++ mmo/users/views.py
@@ -91,6 +91,7 @@ class ProfileUserView(LoginRequiredMixin, DataMixin, UpdateView):
     model = User
     form_class = ProfileUserForm
     template_name = 'users/profile.html'
+    context_object_name = 'profile'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
